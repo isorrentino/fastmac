d8770f97a2bcb6d2b7614ea1299b6e5c79287061
InesSorrentino <43743081+isorrentino@users.noreply.github.com>
2021-04-08 09:55:19 +0200

    Update script-mac.sh

--- script-mac.sh
+++ script-mac.sh
@@ -1,3 +1,9 @@
 # disable spotlight indexing
 sudo mdutil -i off -a
 echo Edit script-mac.sh in your fastmac repo to auto-run commands in your Mac instances
+curl -fsSLo Miniforge3.sh https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-$(uname -m).sh
+bash ./Miniforge3.sh
+~/miniforge3/condabin/conda init
+conda create -n robotologyenv
+conda activate robotologyenv
+conda install -c robotology idyntree icub-main
